88731e1f947bbe7e377f518a99733dad9d919a64
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-11 13:24:43 -0700

    done robot color changer

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -8,21 +8,35 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
 		Robot Spiderman = new Robot();
 		//3. Ask the user what color they would like the robot to draw
+		for(int i =0; i < 30; i ++) {
+		String color = JOptionPane.showInputDialog("Wat color do u want??");
 		
 		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
+if (color.equals("blue")) {
+	Spiderman.setPenColor(0,0,255);
+}
+if (color.equals("red"))  {
+	Spiderman.setPenColor(255,0,0);
+}
+if (color.equals("green")) {
+	Spiderman.setPenColor(0,255,0);
+}
+//6. If the user doesn’t enter anything, choose a random color
+if (color.equals(""))   {
+	Random Sorry = new Random();
+	Spiderman.setPenColor(Sorry.nextInt(255),Sorry.nextInt(255),Sorry.nextInt(255));
+}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		Spiderman.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
         Spiderman.penDown();
         Spiderman.move(100);
@@ -35,4 +49,5 @@ public class RobotColorChooser {
         Spiderman.turn(90);
 
 	}
+}
 }
