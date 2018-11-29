5f59c8b08264a618d73ac4caa13604fb1b0941a7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-29 13:21:37 -0800

    Are you happy done horoscope in progress

--- src/extra/AreYouHappy.java
+++ src/extra/AreYouHappy.java
@@ -6,13 +6,22 @@ public class AreYouHappy {
 	
 public static void main(String[] args) {
 
-		String wants = JOptionPane.showInputDialog(null, "Are you Happy?");
+		String wants = JOptionPane.showInputDialog(null, "Are you happy?");
 
 		if (wants.equalsIgnoreCase("yes")) {
 		JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing:)");
 		}
 		else if (wants.equalsIgnoreCase("no")) {
-		JOptionPane.showMessageDialog(null, "Do you want to be happy?");
-		}
-	}
-}
+		String Wantz = JOptionPane.showInputDialog(null, "Do you want to be happy?");
+		
+	    if(Wantz.equalsIgnoreCase("yes")) {
+	    	JOptionPane.showMessageDialog(null,"Change something");
+	    }
+	  
+	    	else if(Wantz.equalsIgnoreCase("no")) {
+	    JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");	
+	        
+	     }
+	   }
+	 }
+   }
---
+++ src/extra/Horoscope.java
@@ -0,0 +1,19 @@
+package extra;
+
+public class Horoscope {
+public static void main(String[] args) {
+
+	String aries = "Courageous, energetic, willful, commanding, leading. Often leads when following would be best course of action.";
+	String taurus = "Pleasure seeking, loves control, dependable, grounded, provokes slowly, and highly sensual in nature.";
+	String gemini = "Cerebral, chatty, loves learning and education, charming, and adventurous.";
+	String cancer = "Emotional, group oriented, seeks security, family.";		
+	String leo = "Generous,organized, protective, beautiful.";
+	String virgo = "Particular, logical, practical, sense of duty, critical.";
+	String zodiac = "Balanced, seeks beauty, sense of justice.";
+	String scorpio = "Passionate, exacting, loves extremes, combative, reflective.";
+	String sagittarius = "Happy, absent minded, creative, adventurous.";
+	String capricorn = "Timeless, driven, calculating, ambitious.";
+	
+}
+	
+}
