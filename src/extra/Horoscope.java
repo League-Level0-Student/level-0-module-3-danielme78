e4d887f20fba898429f018478a994b12554a7fa9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-06 13:21:45 -0800

    horoscope in progress

--- src/extra/Horoscope.java
+++ src/extra/Horoscope.java
@@ -1,5 +1,7 @@
 package extra;
 
+import javax.swing.JOptionPane;
+
 public class Horoscope {
 public static void main(String[] args) {
 
@@ -13,7 +15,43 @@ public static void main(String[] args) {
 	String scorpio = "Passionate, exacting, loves extremes, combative, reflective.";
 	String sagittarius = "Happy, absent minded, creative, adventurous.";
 	String capricorn = "Timeless, driven, calculating, ambitious.";
-	
+	String aquarius = "Forward thinking, communicative, people oriented, stubborn, generous, and dedicated.";
+	String pisces = "Likeable, energetic, passionate, sensitive.";
+
+String wants = JOptionPane.showInputDialog("What's your star sign?");
+
+if (wants.equalsIgnoreCase("")) {
+	JOptionPane.showMessageDialog(null, );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 	
 }
