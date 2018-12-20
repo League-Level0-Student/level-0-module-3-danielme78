3d24e6aef91a7b15512aca90ca1335d6ea6dfad8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-20 13:22:36 -0800

    Holliday done

---
+++ src/Hollidays.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Hollidays {
+public static void main(String[] args) {
+
+JOptionPane.showMessageDialog(null, "This Message is from the North Pole.");
+
+JOptionPane.showMessageDialog(null, "Santa Cluas wants to know what you want. ");
+
+String gift = JOptionPane.showInputDialog("The Elfs are asking if could tell us underneath.");
+
+JOptionPane.showMessageDialog(null, "Thanks you for helping us Christmas would be much easier now.");
+
+JOptionPane.showMessageDialog(null, "All of the Elfs are working to get you a");
+
+JOptionPane.showMessageDialog(null, gift);
+
+JOptionPane.showMessageDialog(null, "Everyone here at the North Pole are wishing you Happy Holidays!");
+
+}
+}
--- src/extra/AreYouHappy.java
+++ src/extra/AreYouHappy.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
 
 		String wants = JOptionPane.showInputDialog(null, "Are you happy?");
 
-		if (wants.equalsIgnoreCase("yes")) {
-		JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing:)");
+		if (wants.equalsIgnoreJOptionPane.showMessageDialog(null, "Keep doing whatCase("yes")) {
+		ever you're doing:)");
 		}
 		else if (wants.equalsIgnoreCase("no")) {
 		String Wantz = JOptionPane.showInputDialog(null, "Do you want to be happy?");
